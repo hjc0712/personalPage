2abfc3bf743e6ba15b9476dc57780bf0a9aa3640
hjc0712 <hjc0712@163.com>
2020-05-03 04:48:51 -0700

    update side column silde movement

--- public/js/home.js
+++ public/js/home.js
@@ -3,8 +3,8 @@
 $(".hideBt").click(() =>{
     console.log("h");
     //
-    $(".leftCol").css("animation","1s leftColSlideout 0s");
-    $(".mainCol").css("animation","1s mainColSlideout 0s");
+    $(".leftCol").css("animation","1.1s leftColSlideout 0s");
+    $(".mainCol").css("animation","1.1s mainColSlideout 0s");
     $(".hideBt").css("display","none");
     $(".showBt").css("display","inline");
     setTimeout(function(){
@@ -15,13 +15,13 @@ $(".hideBt").click(() =>{
 
 $(".showBt").click(() =>{
     console.log("s");
-    $(".leftCol").css("animation","1s leftColSlidein 0s");
-    $(".mainCol").css("animation","1s mainColSlidein 0s");
+    $(".leftCol").css("animation","1.1s leftColSlidein 0s");
+    $(".mainCol").css("animation","1.1s mainColSlidein 0s");
     $(".leftCol").css("display","inline");
     $(".showBt").css("display","none");
     setTimeout(function(){
         $(".leftCol").css("display","inline");
         $(".hideBt").css("display","inline");
-        $(".mainCol").css("margin-left","150px");
+        $(".mainCol").css("margin-left","100px");
     },1000);
 })
