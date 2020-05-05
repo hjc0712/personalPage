ae6148355e872f5c78ca3c44b621a884e89db367
hjc0712 <hjc0712@163.com>
2020-05-05 08:33:11 -0700

    refactor leftcolumn html & js(css not yet), and add left column to sub page(only profile for now)

--- public/js/leftColumn.js
+++ public/js/leftColumn.js
@@ -1,9 +1,10 @@
-
+// Global Parameters ----------------------------------------
 var newsCount = [0,0,0];
 var globalData = [];
 var weatherIcon =
 
-// Ajax call for web crawler
+// Events ---------------------------------------------------
+// Onloading Ajax call for web crawler
 $( document ).ready(function(){
     //Perform Ajax request.
 
@@ -29,9 +30,75 @@ $( document ).ready(function(){
     });
 });
 
+// left side colunm events
+$(".hideBt").click(() =>{
+    console.log("h");
+    //
+    $(".leftCol").css("animation","1.1s leftColSlideout 0s");
+    $(".mainCol").css("animation","1.1s mainColSlideout 0s");
+    $(".hideBt").css("display","none");
+    $(".showBt").css("display","inline");
+    setTimeout(function(){
+        $(".leftCol").css("display","none");
+        if($("body").width() < 1100) {
+            $(".mainCol").css("margin-left", "0px");
+        }else {
+            $(".mainCol").css("margin-left", "0px");
+        }
+    },1000);
+})
+
+$(".showBt").click(() =>{
+    console.log("s");
+    $(".leftCol").css("animation","1.1s leftColSlidein 0s");
+    $(".mainCol").css("animation","1.1s mainColSlidein 0s");
+    $(".leftCol").css("display","inline");
+    $(".showBt").css("display","none");
+    setTimeout(function(){
+        $(".leftCol").css("display","inline");
+        $(".hideBt").css("display","inline");
+        if($("body").width() < 1100) {
+            $(".mainCol").css("margin-left", "0px");
+        } else if($("body").width() < 1350) {
+            $(".mainCol").css("margin-left", "100px");
+        } else {
+            $(".mainCol").css("margin-left", "250px");
+        }
+    },1000);
+})
+
+// More news button
+$(".news #no1 button").click(() => {
+    newsCount[0] ++;
+    updateNews(globalData[0], 1);
+})
+
+$(".news #no2 button").click(() => {
+    newsCount[1] ++;
+    updateNews(globalData[1], 2);
+})
+
+$(".news #no3 button").click(() => {
+    newsCount[2] ++;
+    updateNews(globalData[2], 3);
+})
+
+// Change weather city button
+$(".changeCity button").click(() => {
+    var input = $("#cityInput");
+    var city = input.val();
+    weatherCrawlerAjax(city);
+
+    input.val('');
+    $(".weather img").removeClass("hidden"); // Reveal the loading img
+    $(".weather .piece").addClass("hidden"); // Clear the preview weather results.
+})
 
 
 
+
+
+// JS functions--------------------------------------------------
 function weatherCrawlerAjax(city){
     $.ajax({
         url: '/home/weatherCrawler',
@@ -51,26 +118,8 @@ function weatherCrawlerAjax(city){
     });
 }
 
-function getWeatherIconSrc(wea) {
-    if(wea.endsWith("Rain")){
-        return "/static/img/index/rainy.png";
-    } else if(wea.endsWith("Partly Cloudy")) {
-        return "/static/img/index/partly_cloudy.png";
-    } else if(wea.endsWith("Cloudy")){
-        return "/static/img/index/cloudy.png";
-    } else if(wea.endsWith("Sunny")){
-        return "/static/img/index/sunny.png";
-    } else if(wea.endsWith("Snow")){
-        return "/static/img/index/snowy.png";
-    } else if(wea.endsWith("Showers")){
-        return "/static/img/index/showers.png";
-    } else if(wea.endsWith("Thunderstorms")){
-        return "/static/img/index/thunders.png";
-    } else {
-        return "/static/img/index/partly_cloudy.png";
-    }
-}
 
+// Called by the weatherCrawler Ajax
 function updateWeather (data, city) {
     var date = data[3];
     var wea = data[0];
@@ -134,65 +183,22 @@ function updateNews (news, number){
     $(selector).html(a2);
 }
 
-
-// left side colunm events
-$(".hideBt").click(() =>{
-    console.log("h");
-    //
-    $(".leftCol").css("animation","1.1s leftColSlideout 0s");
-    $(".mainCol").css("animation","1.1s mainColSlideout 0s");
-    $(".hideBt").css("display","none");
-    $(".showBt").css("display","inline");
-    setTimeout(function(){
-        $(".leftCol").css("display","none");
-        if($("body").width() < 1100) {
-            $(".mainCol").css("margin-left", "0px");
-        }else {
-            $(".mainCol").css("margin-left", "0px");
-        }
-    },1000);
-})
-
-$(".showBt").click(() =>{
-    console.log("s");
-    $(".leftCol").css("animation","1.1s leftColSlidein 0s");
-    $(".mainCol").css("animation","1.1s mainColSlidein 0s");
-    $(".leftCol").css("display","inline");
-    $(".showBt").css("display","none");
-    setTimeout(function(){
-        $(".leftCol").css("display","inline");
-        $(".hideBt").css("display","inline");
-        if($("body").width() < 1350) {
-            $(".mainCol").css("margin-left", "100px");
-        } else {
-            $(".mainCol").css("margin-left", "250px");
-        }
-    },1000);
-})
-
-// More news button
-$(".news #no1 button").click(() => {
-    newsCount[0] ++;
-    updateNews(globalData[0], 1);
-})
-
-$(".news #no2 button").click(() => {
-    newsCount[1] ++;
-    updateNews(globalData[1], 2);
-})
-
-$(".news #no3 button").click(() => {
-    newsCount[2] ++;
-    updateNews(globalData[2], 3);
-})
-
-// Change weather city button
-$(".changeCity button").click(() => {
-    var input = $("#cityInput");
-    var city = input.val();
-    weatherCrawlerAjax(city);
-
-    input.val('');
-    $(".weather img").removeClass("hidden"); // Reveal the loading img
-    $(".weather .piece").addClass("hidden"); // Clear the preview weather results.
-})
+function getWeatherIconSrc(wea) {
+    if(wea.endsWith("Rain")){
+        return "/static/img/index/rainy.png";
+    } else if(wea.endsWith("Partly Cloudy")) {
+        return "/static/img/index/partly_cloudy.png";
+    } else if(wea.endsWith("Cloudy")){
+        return "/static/img/index/cloudy.png";
+    } else if(wea.endsWith("Sunny")){
+        return "/static/img/index/sunny.png";
+    } else if(wea.endsWith("Snow")){
+        return "/static/img/index/snowy.png";
+    } else if(wea.endsWith("Showers")){
+        return "/static/img/index/showers.png";
+    } else if(wea.endsWith("Thunderstorms")){
+        return "/static/img/index/thunders.png";
+    } else {
+        return "/static/img/index/partly_cloudy.png";
+    }
+}
